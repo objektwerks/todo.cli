2d7882445906442a2bc8fd56feb46ff81d5b0537
objektwerks <objektwerks@runbox.com>
2025-03-13 13:16:34 -0400

    enhanvced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,6 +1,10 @@
 package objektwerks
 
-object App:
+import com.typesafe.scalalogging.LazyLogging
+
+object App extends LazyLogging:
   def main(args: Array[String]): Unit =
     println(s"App args[${args.length}]:")
     args.foreach(println)
+    logger.info(s"App args[${args.length}]:")
+    args.foreach(arg => logger.info(arg))
