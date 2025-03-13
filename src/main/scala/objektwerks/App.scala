6abea5bf0025b2d880a83f9976eb2400be9ee468
objektwerks <objektwerks@runbox.com>
2025-03-13 15:59:19 -0400

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -9,21 +9,21 @@ object App extends LazyLogging:
 
   @main
   def list(): Unit =
-    log("list todos:")
+    log("list")
     store.listTodos().foreach { todo => log(todo.toString) }
 
   @main
   def add( @arg(doc = "Add todo.") todo: String* ): Unit =
     val newTodo = Todo(id = store.nextId(), todo = todo.tail.mkString(" "))
     store.writeTodo(newTodo)
-    log(s"add todo: $newTodo")
+    log(s"add --todo $newTodo")
 
   @main
   def completed( @arg(doc = "Completed todo by id.") id: Int): Unit =
     val todo = store.readTodo(s"$id.json")
     val completedTodo = todo.copy(completed = Todo.datetime())
     store.writeTodo(completedTodo)
-    log(s"completed todo: $completedTodo")
+    log(s"completed --id $completedTodo")
 
   def main(args: Array[String]): Unit =
     ParserForMethods(this).runOrExit(args)
