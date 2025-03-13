d0bf98b84b14696d65df35952c9778c3fa9195d9
objektwerks <objektwerks@runbox.com>
2025-03-13 13:19:24 -0400

    edited store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -5,9 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 class StoreTest extends AnyFunSuite with Matchers:
   test("store"):
-    val todo = Todo.default()
     val store = Store()
-    
+    val todo = Todo.default()    
     store.writeTodo(todo)
     store.readTodo(todo.file) shouldBe todo
     store.listTodos.length should be >= 1
