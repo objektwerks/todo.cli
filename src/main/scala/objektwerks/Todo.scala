e4423f312b22515f2d59a62e6d3850ffc11a4cdc
objektwerks <objektwerks@runbox.com>
2025-03-13 12:38:20 -0400

    edited todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -5,4 +5,7 @@ import java.time.Instant
 object Todo:
   def datetime(): String = Instant.now.toString
 
-final case class Todo(id: Int, todo: String, created: String = datetime(), completed: String = "")
+final case class Todo(id: Int,
+                      todo: String,
+                      created: String = datetime(),
+                      completed: String = "")
