a7aca0ae48148d9fc15bac379b027014bd6ad989
objektwerks <objektwerks@runbox.com>
2025-03-13 12:02:13 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,4 +1,6 @@
 package objektwerks
 
 object App:
-  def main(args: Array[String]): Unit = println(args)
+  def main(args: Array[String]): Unit =
+    println(s"App args:")
+    args.foreach(println)
