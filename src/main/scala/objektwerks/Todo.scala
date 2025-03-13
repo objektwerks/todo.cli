c8318ad3da300be40a894c0b8057293bb95df49c
objektwerks <objektwerks@runbox.com>
2025-03-13 12:39:24 -0400

    added todo ordering

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -3,6 +3,8 @@ package objektwerks
 import java.time.Instant
 
 object Todo:
+  given Ordering[Todo] = Ordering.by[Todo, String](t => t.created).reverse
+
   def datetime(): String = Instant.now.toString
 
 final case class Todo(id: Int,
